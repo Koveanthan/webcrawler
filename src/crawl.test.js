3ff272c830abdb8f9639ad63124e70161c80b68f
Koveanthan <154501772+Koveanthan@users.noreply.github.com>
2024-04-21 16:07:15 +0530

    rearrange src files

--- src/crawl.test.js
+++ src/crawl.test.js
@@ -12,7 +12,7 @@ test("Parse html to links - CASE 1", () => {
     const baseURL = "https://www.google.com"
     const html = "<html><body><a href=\"https://www.google.com/home\">Google</a></body></html>"
     const expected = getURLsFromHTML(html, baseURL) 
-    const actual = ['https://www.google.com/home']
+    const actual = ['www.google.com/home']
     expect(expected).toEqual(actual)
 })
 
@@ -20,6 +20,6 @@ test("Parse html to links - CASE 2", () => {
     const baseURL = "https://www.google.com"
     const html = "<html><body><a href=\"https://www.google.com/home\">Google Home</a><a href=\"https://www.facebook.com/home\">Facebook</a></body></html>"
     const expected = getURLsFromHTML(html, baseURL) 
-    const actual = ['https://www.google.com/home', 'https://www.facebook.com/home']
+    const actual = ['www.google.com/home', 'www.facebook.com/home']
     expect(expected).toEqual(actual)
 })
